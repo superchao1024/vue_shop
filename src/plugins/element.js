a15c64effdf4664e727357c3921cf5a14f3c6ac2
sunChao <928285724@qq.com>
2020-08-18 23:33:44 +0800

    2020/8/18完成登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
+
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 弹框组件挂在到vue原型对象上
+Vue.prototype.$message = Message
