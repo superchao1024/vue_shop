85922037eb64f7ac2fbb76c422d64e036632c000
sunChao <928285724@qq.com>
2020-08-30 10:33:29 +0800

    完成了分类功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import {
   Button, Form, FormItem, Input, Message, Container, Header,
   Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip,
-  Pagination, Dialog, MessageBox, Tag, Tree, Select, Option
+  Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader
 } from 'element-ui'
 
 Vue.use(Button)
@@ -32,6 +32,7 @@ Vue.use(Tag)
 Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Cascader)
 // 弹框组件挂在到vue原型对象上
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
